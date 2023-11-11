a4328d3e1da54988be8750c7ece0f56cb8076bda
Warren Huang <warrenhuang@WarrendeMacBook-Pro.local>
2023-11-11 17:49:00 -0600

    swift modulation

---
+++ swift_modulation/modulation_lora/modulation_lora/ContentView.swift
@@ -0,0 +1,55 @@
+//
+//  ContentView.swift
+//  modulation_lora
+//
+//  Created by Warren Huang on 11/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var frequency: Double = 440.0
+    @State var chirpRate: Double = 50.0
+    @State var time: Float = 0.0 // Initialize to 0
+    @State var spreadingFactor: Int = 12
+
+    var body: some View {
+        VStack {
+            Text("Frequency: \(frequency, specifier: "%.1f") Hz")
+            
+            Text("Chirp Rate: \(chirpRate, specifier: "%.1f") Hz/s")
+            Slider(value: $chirpRate, in: 0...100)
+            
+            Text("Time interval: \(time, specifier: "%.2f") s (0 - 5)")
+            
+            HStack {
+                Text("Spreading Factor: \(spreadingFactor)")
+                Button("−") {
+                    if spreadingFactor > 1 {
+                        spreadingFactor -= 1
+                    }
+                }
+                .padding(.leading, 20)
+                
+                Button("+") {
+                    if spreadingFactor < 12 {
+                        spreadingFactor += 1
+                    }
+                }
+                .padding(.leading, 10)
+            }
+            SoundController(
+                            frequency: $frequency,
+                            chirpRate: $chirpRate,
+                            time: $time,
+                            spreadingFactor: $spreadingFactor,
+                            onFrequencyTimeChange: { frequency, time in
+                                self.frequency = frequency
+                                self.time = time
+                            }
+                        )
+        }
+        .padding()
+    }
+}
+
---
+++ swift_modulation/modulation_lora/modulation_loraUITests/modulation_loraUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  modulation_loraUITestsLaunchTests.swift
+//  modulation_loraUITests
+//
+//  Created by Warren Huang on 11/8/23.
+//
+
+import XCTest
+
+final class modulation_loraUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
